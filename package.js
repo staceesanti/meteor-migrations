b9674f71d6df934e901e0e602cdc8a9c4231d7ed
Emilija Radovanovic <emilija@77-46-141-236.static.isp.telekom.rs>
2023-12-26 12:15:14 +0100

    Updated package version

--- package.js
+++ package.js
@@ -1,28 +1,28 @@
 Package.describe({
-  name: "okgrow:migrations",
-  summary: "Allow zero-downtime database migrations for Meteor",
-  version: "0.1.0",
-  git: "https://github.com/okgrow/meteor-migrations"
+  name: 'okgrow:migrations',
+  summary: 'Allow zero-downtime database migrations for Meteor',
+  version: '0.1.0',
+  git: 'https://github.com/okgrow/meteor-migrations'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0.1");
-  api.use(["mongo", "minimongo"]);
-  api.use(["underscore", "check", "momentjs:moment@2.10.6"]);
+  api.versionsFrom('1.0.2');
+  api.use(['mongo', 'minimongo']);
+  api.use(['underscore', 'check', 'momentjs:moment@2.10.6']);
 
-  api.addFiles("shared/index.js", ["client", "server"]);
+  api.addFiles('shared/index.js', ['client', 'server']);
 
-  api.addFiles("server/index.js", "server");
-  api.addFiles("server/migrations.js", "server");
-  api.addFiles("server/startup.js", "server");
+  api.addFiles('server/index.js', 'server');
+  api.addFiles('server/migrations.js', 'server');
+  api.addFiles('server/startup.js', 'server');
 
-  api.export("Migrations");
+  api.export('Migrations');
 });
 
 Package.onTest(function (api) {
-  api.use("tinytest");
-  api.use("okgrow:migrations");
+  api.use('tinytest');
+  api.use('okgrow:migrations');
 
-  api.addFiles("tests/server/index.js", "server");
-  api.addFiles("tests/server/migrate.js", "server");
+  api.addFiles('tests/server/index.js', 'server');
+  api.addFiles('tests/server/migrate.js', 'server');
 });
